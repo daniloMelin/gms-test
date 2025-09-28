09b15d75b9c1c587b3dce6b2898730d0b7cfb045
Danilo Melin <dmelin88@gmail.com>
2025-09-28 16:01:58 -0300

    feat: ✨ adiciona novos cenários de teste e refatora o script

--- cypress/e2e/cadastro.cy.js
+++ cypress/e2e/cadastro.cy.js
@@ -1,33 +1,44 @@
 /// <reference types="cypress" />
 
-describe.skip("US-012-Funcionalidade: Cadastro de membros", () => {
+import { fakerPT_BR } from '@faker-js/faker';
+
+describe("US-012-Funcionalidade: Cadastro de membros", () => {
 
   beforeEach(() => {
-    cy.visit("http://localhost:3000/");
+    cy.visit("/");
   });
 
-  it.skip("Deve fazer o cadastro de campos obrigatórios", () => {
-    cy.get("#signup-firstname").type("Diego");
-    cy.get("#signup-lastname").type("Silva");
+  const email = fakerPT_BR.internet.email();
 
-    cy.get("#signup-email").type("emailSilva@teste.com");
-    cy.get("#signup-phone").type("11999999999");
-    cy.get("#signup-password").type("Forte@1235");
-
-    cy.get("#signup-button").click();
+  it("Deve fazer o cadastro de campos obrigatórios", () => {    
+    cy.preencherCadastro("Douglas", "Silva", email, "11999999999", "Forte@1235");
     cy.get("#signup-response").should("contain", "Cadastro realizado com sucesso");
   });
 
-    it("Deve preencher o email em formato inválido", () => {
-    cy.get("#signup-firstname").type("Diego");
-    cy.get("#signup-lastname").type("Silva");
+  it("Deve validar mensagem de erro quando email duplicado", () => {
+    cy.preencherCadastro("Douglas", "Silva", email, "11999999999", "Forte@1235");
+    cy.get("#signup-response").should("contain", "Este email já está cadastrado");
+  });
 
-    cy.get("#signup-email").type("emailSilvateste.com");
-    cy.get("#signup-phone").type("11999999999");
-    cy.get("#signup-password").type("Forte@1235");
-
-    cy.get("#signup-button").click();
+  it("Deve validar mensagem de erro quando email estiver no formato inválido", () => {
+    cy.preencherCadastro("Douglas", "Silva", "douglas.com.br", "11999999999", "Forte@1235");
     cy.get("#signup-response").should("contain", "E-mail deve ser um email válido");
   });
 
+  it("Deve validar mensagem de erro com telefone inválido", () => {
+    cy.preencherCadastro("Douglas", "Silva", fakerPT_BR.internet.email(), "1199", "Forte@1235");
+    cy.get("#signup-response").should("contain", "Telefone deve ser um telefone válido");
+  });
+
+  it("Deve validar mensagem de erroquando a senha estiver com menos de 8 caracteres", () => {
+    cy.preencherCadastro("Douglas", "Silva", fakerPT_BR.internet.email(), "11999999999", "Forte");
+    cy.get("#signup-response").should("contain", "Senha deve ter pelo menos 8 caracteres");
+  });
+
+  it("Deve validar mensagem de erro quando a senha não contiver caracteres especiais", () => {
+    cy.preencherCadastro("Douglas", "Silva", fakerPT_BR.internet.email(), "11999999999", "Forte1234");
+    cy.get("#signup-response").should("contain", "Senha deve ter pelo menos 8 caracteres");
+  });
+
 });
+
