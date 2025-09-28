bed286f0bd07fb0bd658b4d90b73e122a74fb50f
Danilo Melin <dmelin88@gmail.com>
2025-09-28 16:00:19 -0300

    feat: ✨ expande cobertura de testes para a busca de filmes

---
+++ cypress/e2e/busca.cy.js
@@ -0,0 +1,43 @@
+/// <reference types="cypress" />
+
+describe("US-001-Funcionalidade: Busca de filmes", () => {
+
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  it("Deve buscar um filme na barra de pesquisa", () => {
+    cy.get("#search-input").type("The Matrix");
+    cy.get("#search-button").click();
+
+    cy.get("#results-section").should("be.visible");
+  });
+
+  it("Deve buscar um filme de uma lista", () => {
+    // Pega o segundo filme do arquivo filmes.json
+    cy.fixture("filmes").then((filmes) => {
+      cy.get("#search-input").type(filmes[1].titulo);
+      cy.get("#search-button").click();
+
+      cy.get("#results-section").should('contain', filmes[1].titulo);
+    });
+  });
+
+  it("Deve buscar todos os filmes da lista", () => {
+    // Itera sobre cada filme no arquivo filmes.json
+    cy.fixture("filmes").each((filmes) => {
+      cy.get("#search-input").clear().type(filmes.titulo);
+      cy.get("#search-button").click();
+
+      cy.get("#results-section").should('contain', filmes.titulo);
+    });
+  });
+
+  it("Deve buscar um filme inexistente na barra de pesquisa", () => {
+    // Busca um filme que não está na lista
+    cy.get("#search-input").type("O Senhor dos Anéis");
+    cy.get("#search-button").click();
+
+    cy.get("#results-section > p").should("contain", "Filme não encontrado.");
+  });
+});
